b5613db9801cd86b076fdeb02af76d3cd75a21fd
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-08-06 16:14:40 +0700

    Set State + Arrow Function

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,28 +8,34 @@ class MyComponent extends React.Component {
       address: 'Hỏi Dân IT',
       age: 26
     };
+    //this : ánh xạ (dại diện) cho obj mà mk tham chiếu , this ở đây là MyComponent
+    //Nhấn vào click me in ra 1 dòng log gì đó
+    handleClick = (event) => {
+        event.preventDefault();
+        // console.log(event.target);//target : in ra cả <button>Click me</button>
+        // console.log(event.target.value);//Click Me
+        // console.log('Click Me');
+        //handleClick không biết this là gì
+        //setState : hàm render tự động được gọi lại
+        this.setState({
+            name: 'Minh Thang',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+    }
+    handleOnMoveOver = (event)=>{
+        console.log(event);
+    }
     //jsx : 1 mk React có . Viết code JS bên trong code HTML
     render () {
         //trả về 1 và chỉ 1 phần tử HTML
         //{Viết code JS trong này}
-        //this : ánh xạ (dại diện) cho obj mà mk tham chiếu , this ở đây là MyComponent
-        //Nhấn vào click me in ra 1 dòng log gì đó
-        function handleClick (event) {
-            event.preventDefault();
-            // console.log(event.target);//target : in ra cả <button>Click me</button>
-            // console.log(event.target.value);//Click Me
-            // console.log('Click Me');
-            console.log(this.state.name);
-        }
-        function handleOnMoveOver(event){
-            console.log(event);
-        }
         return(
             <div>
                 My name is {this.state.name} and I'm from {this.state.address} and I'm {this.state.age} years old
-                <button onClick={handleClick}>Click mee</button>
-                <button onClick={handleOnMoveOver}>Click mee</button>
-                <button onMouseOver={handleOnMoveOver}>Hover mee</button>
+                <button onClick={this.handleClick}>Click mee</button>
+                <button onClick={(event) => {this.handleClick(event)}}>Click mee</button>
+                <button onMouseOver={this.handleOnMoveOver}>Hover mee</button>
             </div>
         )
     }
