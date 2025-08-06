d955615685548189b2712f3dd355cdff67c969bb
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-08-06 15:09:47 +0700

    state

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import MyComponent from './components/MyComponent';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
-const App = () => {
-  const count = useSelector(state => state.counter.count);
-  const dispatch = useDispatch();
+import React from 'react'
 
-  return (
-      <div>
-        Hello world with Minh Thang
-          <MyComponent></MyComponent>
-      </div>
-  );
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                Hello world with Minh Thang
+                <MyComponent></MyComponent>
+            </div>
+        )
+    }
 }
+
+
+// const App = () => {
+//     const count = useSelector(state => state.counter.count);
+//     const dispatch = useDispatch();
+//
+//     return (
+//         <div>
+//             Hello world with Minh Thang
+//             <MyComponent></MyComponent>
+//         </div>
+//     );
+// }
 
 export default App;
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,14 +3,19 @@
 //render : tập hợp khối HTML xong chuyển thành giao diện
 import React from 'react'
 class MyComponent extends React.Component {
+    state = {
+      name : 'Eric',
+      address: 'Hỏi Dân IT',
+      age: 26
+    };
     //jsx : 1 mk React có . Viết code JS bên trong code HTML
     render () {
         //trả về 1 và chỉ 1 phần tử HTML
         //{Viết code JS trong này}
+        //this : ánh xạ (dại diện) cho obj mà mk tham chiếu , this ở đây là MyComponent
         return(
             <div>
-                {Math.random()}
-                My First Component
+                My name is {this.state.name} and I'm from {this.state.address} and I'm {this.state.age} years old
             </div>
         )
     }
