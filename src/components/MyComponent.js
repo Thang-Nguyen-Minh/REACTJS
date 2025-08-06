5a61b218e82134034cef4230977f0d84f1dc5465
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-08-06 15:33:22 +0700

    DOM Events

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -13,9 +13,23 @@ class MyComponent extends React.Component {
         //trả về 1 và chỉ 1 phần tử HTML
         //{Viết code JS trong này}
         //this : ánh xạ (dại diện) cho obj mà mk tham chiếu , this ở đây là MyComponent
+        //Nhấn vào click me in ra 1 dòng log gì đó
+        function handleClick (event) {
+            event.preventDefault();
+            // console.log(event.target);//target : in ra cả <button>Click me</button>
+            // console.log(event.target.value);//Click Me
+            // console.log('Click Me');
+            console.log(this.state.name);
+        }
+        function handleOnMoveOver(event){
+            console.log(event);
+        }
         return(
             <div>
                 My name is {this.state.name} and I'm from {this.state.address} and I'm {this.state.age} years old
+                <button onClick={handleClick}>Click mee</button>
+                <button onClick={handleOnMoveOver}>Click mee</button>
+                <button onMouseOver={handleOnMoveOver}>Hover mee</button>
             </div>
         )
     }
