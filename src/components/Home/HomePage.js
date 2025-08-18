ca0235151a30527792837e552cc334cfb1e7e651
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-08-18 17:04:27 +0700

    Design Homepage Video

--- src/components/Home/HomePage.js
+++ src/components/Home/HomePage.js
@@ -1,7 +1,10 @@
+import videoHomepage from "../../assets/video-homepage.mp4"
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
-            Home Page
+            <video autoPlay muted loop>
+                <source src={videoHomepage} type="video/mp4" />
+            </video>
         </div>
     )
 }
