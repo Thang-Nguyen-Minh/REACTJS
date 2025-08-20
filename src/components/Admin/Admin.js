876929915f1bdc1048152d339e074b7992647df9
Minh Thang <ThangNM.B22DCVT525@stu.ptit.edu.vn>
2025-08-20 22:37:51 +0700

    Axios React Toastify React Paginate

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -2,6 +2,7 @@ import SideBar from "./SideBar";
 import './Admin.scss'
 import {FaBars} from 'react-icons/fa';
 import {useState} from 'react';
+
 const Admin = (props) => {
     const [collapsed, setCollapsed] = useState(false);
     return(
@@ -11,7 +12,7 @@ const Admin = (props) => {
             </div>
             <div className="admin-content">
                 <FaBars onClick={()=>setCollapsed(!collapsed)}/>
-                Kieu Yen
+                Code ReactJS
             </div>
         </div>
     )
